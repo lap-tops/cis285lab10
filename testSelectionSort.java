0e6682e062b0ffee6dec15e35079a493396e4a6c
lap-tops <calebnickens@gmail.com>
2024-04-14 16:55:05 -0400

    Made changes to numbers

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -9,33 +9,34 @@ class testSelectionSort {
 	
 	private SelectionSort sorter = new SelectionSort();
 
-    @Test
-    public void testPositives() {
-        int[] actual = {10, 6, 7, 3, 1};
-        int[] expected = {1, 3, 6, 7, 10};
-        assertArrayEquals(expected, sorter.basicSelectionSort(actual));
-    }
+	@Test
+	public void testPositives() {
+	    int[] actual = {11, 6, 7, 3, 1};
+	    int[] expected = {1, 3, 6, 7, 11}; // Changed 10 to 11
+	    assertArrayEquals(expected, sorter.basicSelectionSort(actual));
+	}
 
-    @Test
-    public void testNegatives() {
-        int[] actual = {-8, -3, -7, -1, -4};
-        int[] expected = {-8, -7, -4, -3, -1};
-        assertArrayEquals(expected, sorter.basicSelectionSort(actual));
-    }
+	@Test
+	public void testNegatives() {
+	    int[] actual = {-8, -2, -7, -1, -4};
+	    int[] expected = {-8, -7, -4, -2, -1}; // Changed -3 to -2
+	    assertArrayEquals(expected, sorter.basicSelectionSort(actual));
+	}
 
-    @Test
-    public void testMixed() {
-        int[] actual = {2, -8, 0, 7, -3, 1};
-        int[] expected = {-8, -3, 0, 1, 2, 7};
-        assertArrayEquals(expected, sorter.basicSelectionSort(actual));
-    }
+	@Test
+	public void testMixed() {
+	    int[] actual = {2, -8, 0, 4, -3, 1};
+	    int[] expected = {-8, -3, 0, 1, 2, 4}; // Changed 7 to 4
+	    assertArrayEquals(expected, sorter.basicSelectionSort(actual));
+	}
 
-    @Test
-    public void testDuplicates() {
-        int[] actual = {2, -1, 2, 3, -1, 0};
-        int[] expected = {-1, -1, 0, 2, 2, 3};
-        assertArrayEquals(expected, sorter.basicSelectionSort(actual));
-    }
+	@Test
+	public void testDuplicates() {
+	    int[] actual = {-2, 1, -2, 3, 1, 0};
+	    int[] expected = {-2, -2, 0, 1, 1, 3}; // Corrected the expected array
+	    assertArrayEquals(expected, sorter.basicSelectionSort(actual));
+	}
+
 	
 
 }
