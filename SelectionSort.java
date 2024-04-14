5b14da3bdad72d25f6537e319b2b35b2551ff1ec
lap-tops <114961694+lap-tops@users.noreply.github.com>
2024-04-14 17:05:40 -0400

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,30 @@
+package cis285Lab10;
+
+public class SelectionSort {
+    private int tempo;
+
+    /** Creates a new instance of SelectionSort */
+    public SelectionSort() {
+    }
+
+    /**
+     * A simple SelectionSort algorithm
+     * pre-condition:
+     * post-condition:
+     * inputs:
+     * outputs:
+     * special conditions:
+     */
+    public int[] basicSelectionSort(int[] x) {
+        for (int i = 0; i < x.length; ++i) {
+            for (int j = i + 1; j < x.length; ++j) {
+                if (x[i] > x[j]) {
+                    tempo = x[i];
+                    x[i] = x[j];
+                    x[j] = tempo;
+                }
+            } // end of inner for loop
+        } // end of outer for loop
+        return x;
+    } // end of basicSelectionSort method
+}
